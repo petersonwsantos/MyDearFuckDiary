645d9fedd7123685cc0ede3d9f9e8262d85312de
Go Peppi   :-) <opeterson@hotmail.com>
2021-03-16 01:07:04 +0000

    test

--- docs/buildsite.sh
+++ docs/buildsite.sh
@@ -4,7 +4,7 @@ set -x
 apt-get update
 apt-get -y install git rsync python3 python3-pip
 
-python3 -m pip install requirements.txt 
+python3 -m pip install requirements.txt --user 
 
 pwd ls -lah
 export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
